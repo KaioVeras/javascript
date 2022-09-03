1fe9cf99a613ed880ca69c1014b5ec0cb3659e72
Kaio Vítor Pinheiro Veras <kaiooveras@gmail.com>
2022-09-03 17:04:10 -0300

    Initial commit

---
+++ variaveis/script.js
@@ -0,0 +1,8 @@
+const primeira_variavel = "Kaio Veras";
+console.log(primeira_variavel);
+console.log(typeof(primeira_variavel));
+
+const numero = 30;
+console.log(numero);
+console.log(typeof(numero)); 
+
