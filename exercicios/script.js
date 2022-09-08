f345a14584202957f87ebfb71e02444a365ac4b5
Kaio Vítor Pinheiro Veras <kaiooveras@gmail.com>
2022-09-08 17:23:07 -0300

    atualização script exercicios

---
+++ exercicios/script.js
@@ -0,0 +1,21 @@
+// Exercício 01
+
+/* const pais = "Brasil";
+const continente = "América do Sul";
+let populacao = 212600000; */
+
+// Exercício 02
+
+/* console.log(pais);
+console.log(continente);
+console.log(populacao); */
+
+// Exercício 03 
+
+/* const eh_brasileiro = true;
+const idioma = "português"; */ 
+
+//Exercício 04
+
+/* console.log(eh_brasileiro);
+console.log(idioma); */ 
--- variavel/script.js
+++ variavel/script.js
@@ -1,26 +1,3 @@
-// Exercício 01
-
-/*  const pais = "Brasil";
-const continente = "América do Sul";
-let populacao = 212600000;  */
-
-// Exercício 02
-
-/* console.log(pais);
-console.log(continente);
-console.log(populacao);   */
-
-// Exercício 03 
-
-/* const eh_brasileiro = true;
-const idioma = "português";  */
-
-//Exercício 04
-
-/* console.log(eh_brasileiro);
-console.log(idioma);  */
-
-
 //Operadores
 
 /* const ano_atual = 2022;
@@ -35,11 +12,11 @@ const ultimo_nome = "Veras";
 
 console.log(primeiro_nome + ultimo_nome);
 
-console.log("Colégio " + "Objetivo " + "Taguatinga - QI"); */
+console.log("Colégio " + "Objetivo " + "Taguatinga - QI");  */
 
-/* //Operadores básicos
+//Operadores básicos
 
-const valor = 10;
+/* const valor = 10;
 console.log(valor % 2);
 console.log(11 % 2);  
 
@@ -60,7 +37,7 @@ console.log(3 + undefined);
 
 console.log(1 + 2 + ' Programdores JavaScript'); 
 
-console.log(1 + (2 + ' Programadores JavaScript'));  */
+console.log(1 + (2 + ' Programadores JavaScript'));   */
 
 
 
