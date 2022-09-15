96e392a6ae55415cd2b56b135e8e0f8e493d41be
Kaio Vítor Pinheiro Veras <kaiooveras@gmail.com>
2022-09-15 16:52:02 -0300

    atualização

--- variavel/script.js
+++ variavel/script.js
@@ -55,9 +55,73 @@ console.log( y == k); // true
 console.log( y === k); //false
 console.log( z != w); //true */
 
+//Concatenação de strings
+
+/*  const primeiro_nome = 'Kaio';
+const ultimo_nome = 'Veras';
+const profissao = 'Estudante';
+
+console.log('Meu nome é ' + primeiro_nome + ' ' + ultimo_nome + ' e sou ' + profissao);
+
+// Templates literais
+
+console.log(`Meu nome é ${primeiro_nome} ${ultimo_nome} e sou ${profissao}`); */   
+
+/* const valor1 = 3;
+const valor2 = 5;
+
+console.log(`A soma é ${valor1 + valor2}`); */
+
+// Bloco if
+
+//const idade = 22;
+
+/* if (idade >= 18) {
+    console.log('Você é maior de idade.');
+} */
+
+//if (idade >= 18) console.log('Você é maior de idade.');
+
+/* if (idade >= 18) {
+    console.log('Você é maior de idade.');
+    console.log(`Sua idade é: ${idade}`);
+} */
 
 
+// Bloco if/else
+// Sempre vai entrar em um dos dois blocos
+
+/* const idade = 14;
+
+if (idade >= 18) {
+    console.log('Você é maior de idade.');
+    console.log('Você acabou de entrar no IF');
+} else {
+    console.log('Você é menor de idade.');
+    console.log('Você acabou de entrar no ELSE');
+} */
 
 
+// Conversão de tipos 
 
+/* const ano_nascimento = '1999';
+console.log(typeof ano_nascimento, ano_nascimento);
+
+const numero = Number(ano_nascimento);
+
+console.log(typeof numero, numero);
+
+console.log(Number('25')); */
+
+/* console.log(Number('Marshal'));
+//retorna um NaN -- Not a Number (Não é um número) */
+
+/* console.log(String(33) + ' é minha idade.');
+
+console.log(33 + ' é minha idade.'); */
+
+/* const coercao = '44' + '12' + 10;
+console.log('44' + '12' + 10 ); */
+
+// Valores verdadeiros/Valores falsos
  
