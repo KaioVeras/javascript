2221ea8f8e8ddf0106bf363c1fcccc41eb293009
Kaio Vítor Pinheiro Veras <kaiooveras@gmail.com>
2022-09-17 23:39:58 -0300

    atualização  curso javascript

---
+++ exercicios/ex002/slide2.js
@@ -0,0 +1,37 @@
+// 1 - Usando a função prompt(), leia um número fornecido pelo usuário. Se for positivo calcule a raiz quadrada e caso
+// seja negativo mostre um console.log com a mensagem “Número inválido”.
+
+// let numero_fornecido = prompt('Digite um número: ');
+// let raiz_quadrada = Math.sqrt(numero_fornecido);
+
+// if ( numero_fornecido > 0) {
+//     console.log(`A raiz quadrada desse número é: ${raiz_quadrada}.`);
+// } else {
+//     console.log('Número inválido.');
+// }
+
+
+// ==============================================================
+
+
+// 2 - Usando a função prompt(), peça para o usuário digitar o salário de um trabalhador e o valor de uma prestação de um empréstimo feito. Se a prestação do empréstimo for maior que 20% do salário mostre um console.log() com a frase
+// “Empréstimo não poderá ser concedido”, caso contrário mostre “Empréstimo pode ser concedido”.
+
+// let valor_salario = prompt('Digite o seu salário: ');
+// let valor_prestacao = prompt('Digite o valor da prestação: ');
+// let vinte_porcento_salario = valor_salario * 20 / 100;
+
+// if (valor_prestacao > vinte_porcento_salario) {
+//     console.log(`Empréstimo não poderá ser concedido.`);
+// } else {
+//     console.log(`Epréstimo pode ser concedido.`);
+// }
+
+
+// ==============================================================
+
+
+
+
+
+
